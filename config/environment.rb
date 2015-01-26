c5d654e1b496ad75afbb055d425336144ec2289b
mgomez <mgomez@acid.cl>
2015-01-26 17:44:39 -0300

    mailing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => 'couponsapplication.herokuapp.com' }
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
